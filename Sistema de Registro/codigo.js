143d5f122a4c6e47d7dc06629a575f086e0a5c03
AgusDevs <109630784+AgusDevs@users.noreply.github.com>
2022-08-02 13:59:02 -0300

    Add files via upload

---
+++ Sistema de Registro/codigo.js
@@ -0,0 +1,46 @@
+
+let cantidad = prompt("¿cuantos alumnos son?");
+
+let alumnosTotales = [];
+
+for (i = 0; i < cantidad; i++) {
+	alumnosTotales[i] = [prompt("Nombre del alumno " + (i+1)),0];
+}
+
+const tomarAsistencia = (nombre,p)=>{
+	let presencia = prompt(nombre);
+	if (presencia == "p" || presencia == "p") {
+		alumnosTotales[p][1]++;
+	}
+}
+
+for (i = 0; i < 30; i++) {
+	for (alumno in alumnosTotales) {
+		tomarAsistencia(alumnosTotales[alumno][0],alumno)
+
+	}
+}
+
+for ( alumno in alumnosTotales) {
+	let resultado = `${alumnosTotales[alumno][0]}:<br>
+	       Asistencias: ${alumnosTotales[alumno][1]}<br>
+	       Ausencias: ${30 - alumnosTotales[alumno][1]}
+	       `;
+	       if (30 - alumnosTotales[alumno][1] >18) {
+	       	resultado+= "Reprobado por inasistencias<br><br>";
+	       } else {
+	       	resultado+= "<br><br>"
+	       }
+document.write(resultado);
+
+}
+
+
+
+
+
+
+
+
+
+
